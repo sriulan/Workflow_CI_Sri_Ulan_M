b4cf769181408c31543b4f85426c75f1bceb5615
Sri Ulan M <58365765+sriulan@users.noreply.github.com>
2026-06-17 13:05:14 +0800

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,9 +1,15 @@
-print("Training selesai")
+import mlflow
 
 accuracy = 0.90
 loss = 0.10
 
+# gunakan active run dari mlflow run
+mlflow.log_param("model_type", "CNN")
+mlflow.log_param("epochs", 1)
+
+mlflow.log_metric("accuracy", accuracy)
+mlflow.log_metric("loss", loss)
+
+print("Training selesai")
 print(f"Accuracy: {accuracy}")
 print(f"Loss: {loss}")
-
-print("Workflow CI berhasil dijalankan")
