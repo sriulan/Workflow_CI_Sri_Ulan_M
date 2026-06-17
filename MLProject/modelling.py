6529f67d948ef2c4e5273e3a408b901d31eefcd9
Sri Ulan M <58365765+sriulan@users.noreply.github.com>
2026-06-17 12:53:36 +0800

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -3,25 +3,23 @@ import mlflow
 # Membuat experiment
 mlflow.set_experiment("Workflow_CI_Training")
 
-with mlflow.start_run():
+mlflow.log_param("model", "cnn")
+mlflow.log_metric("accuracy", 0.90)
 
-    mlflow.log_param("model", "cnn")
-    mlflow.log_metric("accuracy", 0.90)
+# Parameter
+mlflow.log_param("model_type", "CNN")
+mlflow.log_param("epochs", 1)
 
-    # Parameter
-    mlflow.log_param("model_type", "CNN")
-    mlflow.log_param("epochs", 1)
+# Simulasi hasil training
+accuracy = 0.90
+loss = 0.10
 
-    # Simulasi hasil training
-    accuracy = 0.90
-    loss = 0.10
+# Log metric
+mlflow.log_metric("accuracy", accuracy)
+mlflow.log_metric("loss", loss)
 
-    # Log metric
-    mlflow.log_metric("accuracy", accuracy)
-    mlflow.log_metric("loss", loss)
-
-    print("Training selesai")
-    print(f"Accuracy: {accuracy}")
-    print(f"Loss: {loss}")
+print("Training selesai")
+print(f"Accuracy: {accuracy}")
+print(f"Loss: {loss}")
 
 print("Workflow CI berhasil dijalankan")
